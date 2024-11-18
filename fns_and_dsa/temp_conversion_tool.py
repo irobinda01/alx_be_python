626e010a05313bd379aeb51f10e9ae80314d7ef2
nathaniro <nathaniro102@gmail.com>
2024-11-18 17:37:53 +0100

    temperature

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -2,6 +2,19 @@ FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 degree_symbol = "\u00B0"
 
+def validate_temperature(input_value):
+  try:
+    return float(input_value)
+  except:
+    raise ValueError('Invalid temperature. Please enter a numeric value.')
+  
+def validate_unit(input_value):
+  unit = input_value.strip().lower()
+  if unit in ('c', 'f'):
+    return unit
+  else:
+    raise ValueError("Invalid temperature scale. Please enter 'C' for Celsius or 'F' for Fahrenheit.")
+
 def convert_to_celsius(fahrenheit):
   return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
 
@@ -10,14 +23,17 @@ def convert_to_fahrenheit(celsius):
 
 if __name__ == "__main__":
   try:
-    check = input('Is this temperature in Celsius or Fahrenheit? (C/F): ').capitalize()
+    tem_value = input("Enter the temperature to be converted: ")
+    temperature = validate_temperature(tem_value)
 
-    if check == 'C':
-      tem = float(input("Enter the temperature to be converted: "))
-      print(f'{tem}{degree_symbol}C is {convert_to_fahrenheit(tem)}{degree_symbol}F')
-    elif check == 'F':
-      tem = float(input("Enter the temperature to be converted: "))
-      print(f'{tem}{degree_symbol}F is {convert_to_celsius(tem)}{degree_symbol}C')
+    check = input('Is this temperature in Celsius or Fahrenheit? (C/F): ')
+    unit = validate_unit(check)
+
+
+    if unit == 'c':
+      print(f'{temperature}{degree_symbol}C is {convert_to_fahrenheit(temperature)}{degree_symbol}F')
+    elif unit == 'f':
+      print(f'{temperature}{degree_symbol}F is {convert_to_celsius(temperature)}{degree_symbol}C')
     else:
       raise ValueError("Invalid temperature. Please enter a numeric value.")
   except ValueError as e:
