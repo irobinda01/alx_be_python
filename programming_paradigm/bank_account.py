d7d9342666536a36e36d63f88e065c4a44de86a8
nathaniro <nathaniro102@gmail.com>
2024-11-22 13:21:31 +0100

    bank account

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -4,14 +4,12 @@ class BankAccount:
 
   def deposit(self,amount):
     self.__account_balance = self.__account_balance + amount
-    return self.account_balance
   
   def withdraw(self,amount):
     if amount > 0 and self.__account_balance > amount:
       self.__account_balance = self.__account_balance - amount
       return True
-    else:
-      return False
+    return False
   
   def display_balance(self):
     print(f"Current Balance: {self.__account_balance}")
