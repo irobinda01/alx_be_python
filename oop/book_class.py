92b2ce7f2c3972b744d52ecea0544b306b98d2fb
nathaniro <nathaniro102@gmail.com>
2025-01-19 18:44:33 +0100

    magic method

---
+++ oop/book_class.py
@@ -0,0 +1,18 @@
+class Book:
+    def __init__(self, title: str, author: str, year: int):
+        """Constructor to initialize a Book instance."""
+        self.title = title
+        self.author = author
+        self.year = year
+
+    def __del__(self):
+        """Destructor to indicate when a Book instance is deleted."""
+        print(f"Deleting '{self.title}'")
+
+    def __str__(self):
+        """Returns an informal string representation of the book."""
+        return f"'{self.title}' by {self.author}, published in {self.year}"
+
+    def __repr__(self):
+        """Returns an official string representation of the book."""
+        return f"Book('{self.title}', '{self.author}', {self.year})"
